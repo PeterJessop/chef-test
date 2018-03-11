3925f06cc92b12edb2edf5e911d58b2438085f32
Cloud User <centos@ip-172-31-15-253.ec2.internal>
2018-03-11 11:18:11 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'tree'
+package 'ntp'
+package 'git' do
+  action :install
+end
+
+
+file '/etc/motd' do
+  content 'This server is the property of Peter Jessop'
+  owner 'root'
+  group 'root'
+end
+service 'ntpd' do
+  action [:enable, :start]
+end
